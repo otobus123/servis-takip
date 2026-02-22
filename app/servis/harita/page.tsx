a2d06ac961a57ab5b72ab76a87b359afaa11fc20
Hiipota_Ch <37468899+otobus123@users.noreply.github.com>
2026-02-22 09:13:44 +0300

    Update page.tsx

--- app/servis/harita/page.tsx
+++ app/servis/harita/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import React, { useEffect, useState } from 'react';
-import 'leaflet/dist/leaflet.css';
+
 
 export default function HaritaSayfasi() {
   const [vehicles, setVehicles] = useState([]);
